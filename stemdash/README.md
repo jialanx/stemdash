95d67a75215e1439d2f09d05c6c53feca8d3790f
jialanx <jialanxie@gmail.com>
2025-05-02 11:20:51 -0700

    installed packages

--- stemdash/README.md
+++ stemdash/README.md
@@ -1,2 +1,11 @@
 npm install
 npx nodemon
+
+
+packages:
+react-router-dom
+mysql2 
+cors
+dotenv
+express
+bcrypt
--- stemdash/src/App.jsx
+++ stemdash/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        test
+        hello world
       </div>
     </>
   )
